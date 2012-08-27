256be2692a341d0d4742dd476549dee9288f945f
milr <milrzeit@gmail.com>
2012-08-27 10:01:21 -0500

    roll back

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -10,7 +10,7 @@ namespace MvcTestGit.Controllers
 	{
 		public ActionResult Index()
 		{
-			ViewBag.Message = "Modify this template to jump-start your ASP.NET MVC application.";
+			ViewBag.Message = "Moxxdify this template to jump-start your ASP.NET MVC application. xcxcxcxc";
 
 			return View();
 		}
